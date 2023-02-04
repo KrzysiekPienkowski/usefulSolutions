3d65e8308517dc23dd377c98cdce947ca2a1bbf0
Krzysiek <krzysiek.pienkowski@gmail.com>
2023-02-04 12:26:38 +0100

    init project

---
+++ src/test/java/com/kp/dev/usefulsolutions/UsefulSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.dev.usefulsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefulSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
